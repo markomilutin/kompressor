db15cea5b414007ed4a796bc3fe0981266dc9a84
mmilutinovic <markomilutin@gmail.com>
2015-06-24 17:59:44 -0600

    - Added specific symbol expansion function and tests

--- Dekompressor.py
+++ Dekompressor.py
@@ -67,13 +67,53 @@ class Dekompressor:
     def _expandRunsSpecific(self, symbolToExpand, runLengthSymbolStart_, maxRunLength_, incomingData_ , incomingDataSize_, expandedData_, expandedDataMaxSize_):
         """
         Expand runs of symbol passed in based on extended symbols starting at runLengthSymbolStart_ and increases by
-        upto maxRunLength. The data will be expanded
+        upto maxRunLength. The data will be expanded in to the expandedData_ array up to expandedDataMaxSize_
 
-        :param symbolToExpand:
-        :param runLengthSymbolStart_:
-        :param maxRunLength_:
-        :param dataSection:
-        :param dataSize_:
-        :return:
+        :param symbolToExpand: The symbol that will be expanded based on the extended symbols
+        :param runLengthSymbolStart_: The first extended symbol that indicates a run of the symbol passed in
+        :param maxRunLength_: The max run length which indicates the last extended symbol that we can expect
+        :param incomingData_: The incoming data that we are expanding (integer array)
+        :param incomingDataSize_: The size of the incoming data
+        :param expandedData_: Expanded data will be stored here (integer array)
+        :param expandedDataMaxSize_: The max number of symbols that can be stored in expandedData_
+        :return: Return the number of symbols stored in expandedData_
         """
-        pass
+
+        currentIncomingDataIndex = 0
+        currentExpandedDataIndex = 0
+        currentSymbol = 0xFFFF
+
+        while(currentIncomingDataIndex < incomingDataSize_):
+
+            currentSymbol = incomingData_[currentIncomingDataIndex]
+            currentIncomingDataIndex += 1
+
+            # Ensure symbol is in allowed range. Must be between 0-256 or (runLengthSymbolStart_) to (runLengthSymbolStart_ + (maxRunLength - 2))
+            if(((currentSymbol > 255) and (currentSymbol < runLengthSymbolStart_)) or
+               (currentSymbol > (runLengthSymbolStart_ + maxRunLength_))):
+                raise Exception('Symbol [' + str(currentSymbol) + '] out of range')
+
+            # If binary value copy else if this is an extended symbol then expand out the symbol provided. Note the first extended symbol represents a run of two so need to offset by two
+            if(currentSymbol >= runLengthSymbolStart_):
+
+                # The run length is based on the current extended symbol. Find the offset from the base extended symbol and add 2 to reflect that the base represents a run of two
+                runLength = currentSymbol - runLengthSymbolStart_ + 2
+
+                if((currentExpandedDataIndex + runLength) >= expandedDataMaxSize_):
+                    raise Exception('Not enough space in expansion array 1')
+
+                for i in range(0, runLength):
+                    expandedData_[currentExpandedDataIndex] = symbolToExpand
+                    currentExpandedDataIndex += 1
+            else:
+                if((currentExpandedDataIndex + 1) >= expandedDataMaxSize_):
+                    raise Exception('Not enough space in expansion array 2')
+
+                expandedData_[currentExpandedDataIndex] = currentSymbol
+                currentExpandedDataIndex +=1
+
+        return currentExpandedDataIndex
+
+
+
+
--- test_Dekompressor.py
+++ test_Dekompressor.py
@@ -32,8 +32,121 @@ class KompressorTests(unittest.TestCase):
         self.assertNotEqual(None, dekompressor.mDecoder)
         self.assertEqual(False, dekompressor.mContinuousModeEnabled)
         self.assertEqual(0, dekompressor.mContinuousModeTotalData)
-        self.assertEqual(0, dekompressor.mContinuousModeDataCompressed)
+        self.assertEqual(0, dekompressor.mContinuousModeDataDecompressed)
 
+    def test_expandRunsSpecific_small_set(self):
+        """
+        Purpose: Pass in small array to be decoded which has expansion symbols at beginning and end
+        Expectation: The data should be expanded correctly
+        """
+
+        dekompressor = Dekompressor(256, 0x00, 3, 0xFF, 4, 10)
+
+        compressedData = array('i', [257, 2, 0, 4, 260])
+        expandedData = array('i', [0]*32)
+
+        expandedCount = dekompressor._expandRunsSpecific(0, 257, 3, compressedData, 5, expandedData, 20)
+
+        self.assertEqual(10, expandedCount)
+        self.assertEqual(0, expandedData[0])
+        self.assertEqual(0, expandedData[1])
+        self.assertEqual(2, expandedData[2])
+        self.assertEqual(0, expandedData[3])
+        self.assertEqual(4, expandedData[4])
+        self.assertEqual(0, expandedData[5])
+        self.assertEqual(0, expandedData[6])
+        self.assertEqual(0, expandedData[7])
+        self.assertEqual(0, expandedData[8])
+        self.assertEqual(0, expandedData[9])
+
+    def test_expandRunsSpecific_larger_data_set(self):
+        """
+        Purpose: Pass in array to be decoded with expansion symbol at the end
+        Expectation: The data should be expanded correctly
+        """
+
+        dekompressor = Dekompressor(256, 0x00, 3, 0xFF, 4, 10)
+
+        compressedData = array('i', [1, 2, 260, 5, 6, 7, 259, 259, 260, 260, 257, 8, 9, 0, 1, 1, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+        expandedData = array('i', [0]*64)
+
+        expandedCount = dekompressor._expandRunsSpecific(0, 257, 3, compressedData, 27, expandedData, 64)
+
+        self.assertEqual(46, expandedCount)
+        self.assertEqual(1, expandedData[0])
+        self.assertEqual(2, expandedData[1])
+        self.assertEqual(0, expandedData[2])
+        self.assertEqual(0, expandedData[3])
+        self.assertEqual(0, expandedData[4])
+        self.assertEqual(0, expandedData[5])
+        self.assertEqual(0, expandedData[6])
+        self.assertEqual(5, expandedData[7])
+        self.assertEqual(6, expandedData[8])
+        self.assertEqual(7, expandedData[9])
+        self.assertEqual(0, expandedData[10])
+        self.assertEqual(0, expandedData[11])
+        self.assertEqual(0, expandedData[12])
+        self.assertEqual(0, expandedData[13])
+        self.assertEqual(0, expandedData[14])
+        self.assertEqual(0, expandedData[15])
+        self.assertEqual(0, expandedData[16])
+        self.assertEqual(0, expandedData[17])
+        self.assertEqual(0, expandedData[18])
+        self.assertEqual(0, expandedData[19])
+        self.assertEqual(0, expandedData[20])
+        self.assertEqual(0, expandedData[21])
+        self.assertEqual(0, expandedData[22])
+        self.assertEqual(0, expandedData[23])
+        self.assertEqual(0, expandedData[24])
+        self.assertEqual(0, expandedData[25])
+        self.assertEqual(0, expandedData[26])
+        self.assertEqual(0, expandedData[27])
+        self.assertEqual(0, expandedData[28])
+        self.assertEqual(0, expandedData[29])
+        self.assertEqual(8, expandedData[30])
+        self.assertEqual(9, expandedData[31])
+        self.assertEqual(0, expandedData[32])
+        self.assertEqual(1, expandedData[33])
+        self.assertEqual(1, expandedData[34])
+        self.assertEqual(2, expandedData[35])
+        self.assertEqual(0, expandedData[36])
+        self.assertEqual(0, expandedData[37])
+        self.assertEqual(0, expandedData[38])
+        self.assertEqual(0, expandedData[39])
+        self.assertEqual(0, expandedData[40])
+        self.assertEqual(0, expandedData[41])
+        self.assertEqual(0, expandedData[42])
+        self.assertEqual(0, expandedData[43])
+        self.assertEqual(0, expandedData[44])
+        self.assertEqual(0, expandedData[45])
+
+    def test_expandRunsSpecific_symbol_below_expansion_range(self):
+        """
+        Purpose: Pass in data that has an extended symbol that is greater than 255 but less than expansion symbol range
+        Expectation: An exception should be thrown
+        """
+
+        dekompressor = Dekompressor(256, 0x00, 3, 0xFF, 4, 10)
+
+        compressedData = array('i', [256, 2, 0, 4, 260])
+        expandedData = array('i', [0]*32)
+
+        with self.assertRaises(Exception):
+            expandedCount = dekompressor._expandRunsSpecific(0, 257, 3, compressedData, 5, expandedData, 20)
+
+    def test_expandRunsSpecific_symbol_above_expansion_range(self):
+        """
+        Purpose: Pass in data that has an extended symbol that is greater than expansion symbol range
+        Expectation: An exception should be thrown
+        """
+
+        dekompressor = Dekompressor(256, 0x00, 3, 0xFF, 4, 10)
+
+        compressedData = array('i', [256, 2, 0, 4, 264])
+        expandedData = array('i', [0]*32)
+
+        with self.assertRaises(Exception):
+            expandedCount = dekompressor._expandRunsSpecific(0, 257, 3, compressedData, 5, expandedData, 20)
 
 if __name__ == '__main__':
     unittest.main()
